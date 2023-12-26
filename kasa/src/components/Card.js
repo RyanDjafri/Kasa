a70f88390b823af741378bdb202ff45d370de2db
RyanDjafri <ryandjafripro@gmail.com>
2023-12-26 12:05:40 +0100

    cards-container

--- kasa/src/components/Card.js
+++ kasa/src/components/Card.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Card = () => {
-  return <div></div>;
+const Card = ({ location }) => {
+  return (
+    <div className="card">
+      <article>
+        <img
+          src={location.cover}
+          alt={"cover of " + location.title}
+          className="card-img"
+        />
+        <h3 className="card-title">{location.title}</h3>
+      </article>
+    </div>
+  );
 };
 
 export default Card;
--- kasa/src/pages/Home.js
+++ kasa/src/pages/Home.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logokasa.svg";
 import { Link } from "react-router-dom";
 import BgPhoto from "../assets/bg.jpg";
+import data from "../logements.json";
+import Card from "../components/Card";
 
 const Home = () => {
+  const [locations, setLocations] = useState(data);
+
   return (
-    <div className="home-container">
-      <header>
-        <img src={Logo} alt="kasa-logo" />
-        <nav>
-          <Link to="/">Accueil</Link>
-          <Link to="/about">A Propos</Link>
-        </nav>
-      </header>
+    <div>
+      <div className="header-container">
+        <header>
+          <img src={Logo} alt="kasa-logo" />
+          <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/about">A Propos</Link>
+          </nav>
+        </header>
+      </div>
       <div className="bg-container">
         <img src={BgPhoto} alt="title-img" />
+      </div>
+      <div className="cards-container">
+        {locations &&
+          locations.map((location) => {
+            return <Card key={location.id} location={location} />;
+          })}
       </div>
     </div>
   );
