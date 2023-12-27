d2a78f1c2a33ec2fa3b791414997c69ab5710a76
RyanDjafri <ryandjafripro@gmail.com>
2023-12-27 13:51:47 +0100

    about

--- kasa/src/pages/About.js
+++ kasa/src/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import BgPhoto from "../assets/kalen.png";
+import cross from "../assets/cross-up.svg";
 
 const About = () => {
   return (
@@ -11,44 +12,53 @@ const About = () => {
       </div>
       <div className="opening-divs">
         <div className="opening-div">
-          <p className="div-title">
-            Flabilité<i class="fa-solid fa-arrow-down"></i>
-            <div className="div-text">
-              Les annonces postées sur Kasa garantissent une fiabilité totale.
-              Les photos sont conformes aux logements, et toutes les
-              informations sont régulièrement vérifiées par nos équipes.
-            </div>
-          </p>
+          <div className="open">
+            <p className="div-title">Fiabilité </p>
+            <img src={cross} alt="cross-up" className="cross" />
+          </div>
+          <div className="div-text">
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes.
+          </div>
         </div>
         <div className="opening-div">
-          <p className="div-title">
-            Respect<i class="fa-solid fa-arrow-down"></i>
-            <div className="div-text">
-              {/* Les annonces postées sur Kasa garantissent une fiabilité totale.
-              Les photos sont conformes aux logements, et toutes les
-              informations sont régulièrement vérifiées par nos équipes. */}
-            </div>
-          </p>
+          <div className="open">
+            <p className="div-title">Respect</p>
+            <img src={cross} alt="cross-up" className="cross" />
+          </div>
+          <div className="div-text">
+            La bienveillance fait partie des valeurs fondactrices de Kasa. Tout
+            comportement discriminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme.
+          </div>
         </div>
         <div className="opening-div">
-          <p className="div-title">
-            Service<i class="fa-solid fa-arrow-down"></i>
-            <div className="div-text">
-              {/* Les annonces postées sur Kasa garantissent une fiabilité totale.
-              Les photos sont conformes aux logements, et toutes les
-              informations sont régulièrement vérifiées par nos équipes. */}
-            </div>
-          </p>
+          <div className="open">
+            <p className="div-title">
+              Service
+              <img src={cross} alt="cross-up" className="cross" />
+            </p>
+          </div>
+          <div className="div-text">
+            La bienveillance fait partie des valeurs fondactrices de Kasa. Tout
+            comportement discriminatoire ou de perturbation du voisinage
+            entraînera de notre plateforme.
+          </div>
         </div>
         <div className="opening-div">
-          <p className="div-title">
-            Sécurité<i class="fa-solid fa-arrow-down"></i>
-            <div className="div-text">
-              {/* Les annonces postées sur Kasa garantissent une fiabilité totale.
-              Les photos sont conformes aux logements, et toutes les
-              informations sont régulièrement vérifiées par nos équipes. */}
-            </div>
-          </p>
+          <div className="open">
+            <p className="div-title">Sécurité</p>
+            <img src={cross} alt="cross-up" className="cross" />
+          </div>
+          <div className="div-text">
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+            pour les voyageurs, chaque logement correspond aux critères de
+            sécurité établis par nos services. En laissant une note aussi bien à
+            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
+            les standards sont bien respectés. Nous organisons également des
+            ateliers sur la sécurité domestique pour nos hôtes.
+          </div>
         </div>
       </div>
     </div>
