8034b47eaaf577ccd536d05736f39d08236e6bd3
RyanDjafri <ryandjafripro@gmail.com>
2024-03-14 17:00:19 +0100

    responsive

--- kasa/src/components/Footer.js
+++ kasa/src/components/Footer.js
@@ -6,21 +6,19 @@ import A from "../assets/a.svg";
 
 const Footer = () => {
   return (
-    <div>
-      <footer>
-        <div className="footer-div">
-          <div className="logo">
-            <img src={Vector} alt="vector-logo" className="vector" />
-            <img src={Group} alt="group-icon" className="group" />
-            <img src={S} alt="footer-s-icon" className="s-logo" />
-            <img src={A} alt="footer-a-icon" className="a-logo" />
-          </div>
-          <div className="text">
-            <p className="footer-text">© 2020 Kasa. All rights reserved</p>
-          </div>
+    <footer>
+      <div className="footer-div">
+        <div className="logo">
+          <img src={Vector} alt="vector-logo" className="vector" />
+          <img src={Group} alt="group-icon" className="group" />
+          <img src={S} alt="footer-s-icon" className="s-logo" />
+          <img src={A} alt="footer-a-icon" className="a-logo" />
         </div>
-      </footer>
-    </div>
+        <div className="text">
+          <p className="footer-text">© 2020 Kasa. All rights reserved</p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
